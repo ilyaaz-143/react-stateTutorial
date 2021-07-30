5e08798269c07671c5161d8d8c08a419f81c62a3
ilyaaz-143 <farxiyoxass@gmail.com>
2021-07-30 21:35:58 +0300

    react state test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import * as MaIcons from "react-icons/md";
+import Icon from "@material-ui/core/Icon";
+// import { classes } from "istanbul-lib-coverage";
+const useStyle = makeStyles((themes) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: themes.spacing(1),
+    },
+  },
+}));
 function App() {
+  const classes = useStyle();
+  const [inputField, setInputfield] = useState([{ Name: "", Email: "" }]);
+  const handelChange = (index, event) => {
+    console.log(index, event.target.name);
+    const values = [...inputField];
+    values[index][event.target.name] = event.target.value;
+    setInputfield(values);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let arrNames = [];
+    let arrEmails = [];
+    for (let i = 0; i < inputField.length; i++) {
+      arrNames.push(inputField[i].Name);
+      arrEmails.push(inputField[i].Email);
+    }
+    console.log(arrNames, arrEmails);
+  };
+  const handleAdd = () => {
+    setInputfield([...inputField, { Name: "", Email: "" }]);
+  };
+  const handleRemove = (index) => {
+    const values = [...inputField];
+    if (values.length > 1) {
+      values.splice(index, 1);
+    } else {
+      alert("cannot remove");
+    }
+    setInputfield(values);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <h1>Add Member</h1>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        {inputField.map((inputfill, index) => {
+          return (
+            <div key={index} className="divinput">
+              <TextField
+                type="text"
+                name="Name"
+                label="Name"
+                required
+                value={inputfill.Name}
+                onChange={(event) => handelChange(index, event)}
+              />
+              <TextField
+                name="Email"
+                label="Email"
+                value={inputfill.Email}
+                required
+                onChange={(event) => handelChange(index, event)}
+              />
+              <IconButton onClick={() => handleRemove(index)}>
+                <MaIcons.MdRemove />
+              </IconButton>
+              <IconButton onClick={() => handleAdd()}>
+                <MaIcons.MdAdd />
+              </IconButton>
+            </div>
+          );
+        })}
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          endIcon={<MaIcons.MdSend />}
+          onClick={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Send
+        </Button>
+      </form>
+    </Container>
   );
 }
 
